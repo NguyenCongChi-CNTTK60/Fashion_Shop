689e5294737e69ce0ef3ebf14307f15998738959
buivantan01 <buivantan1234566@gmail.com>
2021-10-25 12:28:15 +0700

    Refactor: handle section coupon

--- BUS/CouponBUS.cs
+++ BUS/CouponBUS.cs
@@ -22,6 +22,33 @@ namespace BUS
             get { if (instance == null) instance = new CouponBUS(); return instance; }
             set => instance = value;
         }
+
+        public DataTable GetListCoupon()
+        {
+            return CouponDAO.Intance.GetListCoupon();
+        }
+
+        public bool themPN(string maPN, string maNCC, DateTime NgayNhap, string TenDangNhap)
+        {
+
+            return CouponDAO.Intance.themPN(maPN, maNCC, NgayNhap, TenDangNhap);
+        }
+
+        public string loadMaPN()
+        {
+            return CouponDAO.Intance.loadMaPN();
+        }
+
+        public bool xoaPN(string maPN)
+        {
+            return CouponDAO.Intance.xoaPN(maPN);
+        }
+
+
+        public DataTable TimKiemPN(string maPN)
+        {
+            return CouponDAO.Intance.TimKiemPN(maPN);
+        }
     }
 }
 
