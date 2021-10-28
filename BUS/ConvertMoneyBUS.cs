1baa315426438f618d5bb4b968dba5cfb0b87cb7
buivantan01 <buivantan1234566@gmail.com>
2021-10-28 09:37:06 +0700

    Refactor: create convert money BUS

---
+++ BUS/ConvertMoneyBUS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+
+namespace BUS
+{
+    public class ConvertMoneyBUS
+    {
+        private static ConvertMoneyBUS instance;
+
+        public ConvertMoneyBUS()
+        {
+
+        }
+        public static ConvertMoneyBUS Instance
+        {
+            get { if (instance == null) instance = new ConvertMoneyBUS(); return ConvertMoneyBUS.instance; }
+            private set { ConvertMoneyBUS.instance = value; }
+        }
+
+        public string chuyenDoi(double gnumber)
+        {
+            return ConvertMoneyDAO.Instance.So_chu(gnumber);
+        }
+    }
+}
